4e823a3061ba282196d9d42f832148b6bf917b24
MHAE391 <modatherelgohary391@gmail.com>
2021-06-20 15:55:11 +0200

    Fist Task

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.ADD(50);
+            student.ADD(55);
+            student.ADD(22.5);
+            student.ADD(100);
+            student.ADD(99.5);
+            student.print();
+
+        }
+    }
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+	public class Student
+	{
+
+		private List <double> grades = new List<double>();
+		public void ADD(double grade)
+        {
+			if (grade >= 0 && grade <= 100) grades.Add(grade);
+			else Console.WriteLine("Invalid Grade!!");
+        }
+        public void print()
+        {
+            Console.WriteLine($"Min Grade = { grades.Min()}");
+            Console.WriteLine($"Max Grade = { grades.Max()}");
+            Console.WriteLine($"Grades AVG = { grades.Average()}");
+
+        }
+
+    }
+}
